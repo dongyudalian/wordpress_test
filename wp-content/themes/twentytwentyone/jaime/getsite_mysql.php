49267b8acc2d523e5cd139d2dd14d818f269448a
unknown <U676680@bxo.local>
2021-07-30 16:10:50 +0900

    add img

--- wp-content/themes/twentytwentyone/jaime/getsite_mysql.php
+++ wp-content/themes/twentytwentyone/jaime/getsite_mysql.php
@@ -14,11 +14,13 @@ $table = "<table border='1'>
 <th>ID</th>
 <th>content</th>
 <th>title</th>
+<th>image</th>
 </tr>";
 $table .= "<tr>";
 $table .=  "<td class = 'sql_id'>" . $result[0]->ID . "</td>";
 $table .=  "<td>" . $result[0]->post_content . "</td>";
-$table .=  "<td class = 'sql_id'>" . $result[0]->post_title . "</td>";
+$table .=  "<td>" . $result[0]->post_title . "</td>";
+$table .=  "<td class = 'sql_id'>" . get_the_post_thumbnail($result[0]->ID) . "</td>";
 $table .=  "</tr>";
 $table .=  "</table>";
 

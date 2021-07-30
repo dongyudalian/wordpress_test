e749346988f0de4cf02baf46acd7a01e5db45b85
unknown <U676680@bxo.local>
2021-07-30 14:29:00 +0900

    add sql_ajax

--- wp-content/themes/twentytwentyone/ajax/jaime_ajax.php
+++ wp-content/themes/twentytwentyone/ajax/jaime_ajax.php
@@ -31,6 +31,35 @@ function jaime_ajax_all() {
 			xmlhttp.open("GET","ajax_used.php?q="+str,true);
 			xmlhttp.send();
 		}
+
+		function showSite(str)
+		{
+			if (str=="")
+			{
+				document.getElementById("txtHint_sql").innerHTML="";
+				return;
+			} 
+			if (window.XMLHttpRequest)
+			{
+				// IE7+, Firefox, Chrome, Opera, Safari 浏览器执行代码
+				xmlhttp=new XMLHttpRequest();
+			}
+			else
+			{
+				// IE6, IE5 浏览器执行代码
+				xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+			}
+			xmlhttp.onreadystatechange=function()
+			{
+				if (xmlhttp.readyState==4 && xmlhttp.status==200)
+				{
+					document.getElementById("txtHint_sql").innerHTML=xmlhttp.responseText;
+				}
+			}
+			xmlhttp.open("GET","getsite_mysql.php?q="+str,true);
+			xmlhttp.send();
+		}
+
     </script>
 <?php
 }
